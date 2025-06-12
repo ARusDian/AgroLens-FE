6c8feb32b738123ed306f0401488bf4ecb65f402
RUVNE <dian.galleon@gmail.com>
2025-06-13 04:11:37 +0800

    linting error lagi terus

--- src/app/diagnosa/page.tsx
+++ src/app/diagnosa/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useChat } from '@/hooks/useChat';
 import { useImageUpload } from '@/hooks/useImageUpload';
 import { ImageUploader } from '@/components/diagnosa/ImageUploader';
